79ff27dd29365afb61fb6fd4cca2e9b2ada7ab1c
dgallegos <dgallegos@agroasemex.gob.mx>
2022-07-27 14:12:22 -0500

    Cambios iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,37 @@
 import UIKit
 import Flutter
+import workmanager
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
+  
+    /*
+    override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    WorkmanagerPlugin.registerTask(withIdentifier: "tagobtieneasignacionesauto")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+  }*/
+    
+    override func application(
+           _ application: UIApplication,
+           didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+           GeneratedPluginRegistrant.register(with: self)
+           UNUserNotificationCenter.current().delegate = self
+
+           WorkmanagerPlugin.setPluginRegistrantCallback { registry in
+               // Registry in this case is the FlutterEngine that is created in Workmanager's
+               // performFetchWithCompletionHandler or BGAppRefreshTask.
+               // This will make other plugins available during a background operation.
+               GeneratedPluginRegistrant.register(with: registry)
+           }
+
+           WorkmanagerPlugin.registerTask(withIdentifier: "tagobtieneasignacionesauto")
+
+           return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+
+       }
 }
